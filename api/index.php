256396c93d77a3467fb94536c9d3e6eb44322a53
Amad-vav <241230019@unmuhpnk.ac.id>
2026-06-10 19:47:12 +0700

    chore: redirect storage path to /tmp for Vercel compatibility

--- api/index.php
+++ api/index.php
@@ -1,2 +1,15 @@
 <?php
+// Pastikan folder storage sementara ada di direktori /tmp yang bisa ditulisi
+$storageDirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/logs',
+];
+foreach ($storageDirs as $dir) {
+    if (!file_exists($dir)) {
+        @mkdir($dir, 0755, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
